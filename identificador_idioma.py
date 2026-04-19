101d6ca6c3b2d295403a64e98950454c7940a11e
isadoracarrari <isadoracarrari@gmail.com>
2026-04-19 16:31:32 -0300

    feat: adiciona identificador de idioma por frequência de letras

---
+++ identificador_idioma.py
@@ -0,0 +1,11 @@
+#Entra na pasta do projeto
+cd identificador-idioma
+
+# Inicializa o Git
+git init
+
+# Adiciona todos os arquivos para serem rastreados
+git add .
+
+# Cria o primeiro commit (snapshot do projeto)
+git commit -m "feat: adiciona identificador de idioma por frequência de letras"
